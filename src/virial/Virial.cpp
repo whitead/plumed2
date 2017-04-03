6791c6924f2075b91cf7f3d58958953e1e693d68
Andrew White <white.d.andrew@gmail.com>
2017-04-03 11:38:23 -0400

    Virial CV now only segfaults at deconstructor. Basically success

--- src/virial/Virial.cpp
+++ src/virial/Virial.cpp
@@ -1,7 +1,6 @@
-
 #include "colvar/Colvar.h"
 #include "core/ActionRegister.h"
-#include "core/ActionAtomsRegister.h"
+#include "core/ActionAtomistic.h"
 #include "tools/Pbc.h"
 #include "tools/LinkCells.h"
 #include "tools/AtomNumber.h"
@@ -45,7 +44,7 @@ Calculate ?
 class Virial : public Colvar {
 private:
   //if we are computing self-virial
-  bool self_virial;
+  bool b_self_virial;
   //Neighbor list linkscells to accelerate
   LinkCells linkcells;
   //atoms
@@ -54,41 +53,43 @@ private:
   //used to store neighboring atoms
   vector<unsigned int> neighs;
 
+  void setupLinkCells();  
+
 public:
   static void registerKeywords( Keywords& keys );
   explicit Virial(const ActionOptions&);
 // active methods:
   virtual void calculate();
+  virtual void prepare();
 };
 
-PLUMED_REGISTER_ACTION(Virial,"Virial")
+PLUMED_REGISTER_ACTION(Virial,"VIRIAL")
 
 void Virial::registerKeywords( Keywords& keys ){
   Colvar::registerKeywords(keys);
 
   keys.add("compulsory", "CUTOFF", "The maximum distance to consider for pairwise calculations");
 
-  keys.reset_style("ATOMS","atoms");
+  //  keys.reset_style("ATOMS","atoms");
   keys.add("atoms-1","GROUP","Calculate the distance between each distinct pair of atoms in the group");
   keys.add("atoms-2","GROUPA","Calculate the distances between all the atoms in GROUPA and all "
                               "the atoms in GROUPB. This must be used in conjuction with GROUPB.");
   keys.add("atoms-2","GROUPB","Calculate the distances between all the atoms in GROUPA and all the atoms "
                               "in GROUPB. This must be used in conjuction with GROUPA.");
-
-  addValueWithDerivatives();
 }
 
-Virial::Distances(const ActionOptions&ao):
-Action(ao),
-self_virial(false),
-linkcells(comm),
+Virial::Virial(const ActionOptions&ao):
+PLUMED_COLVAR_INIT(ao),
+b_self_virial(false),
+neighs(0),
+linkcells(comm)
 {
   //used for link list
   double cutoff;
   // Read in the atoms
   vector< vector<AtomNumber> > all_atoms(3);
 
-  parseFlag("CUTOFF", cutoff);
+  parse("CUTOFF", cutoff);
   parseAtomList("GROUP", all_atoms[0]);
   parseAtomList("GROUPA", all_atoms[1]);
   parseAtomList("GROUPB", all_atoms[2]);
@@ -97,43 +98,50 @@ linkcells(comm),
     if(all_atoms[1].size() < 2 && all_atoms[2].size() < 2) {
       error("Must specify both GROUPA and GROUPB or GROUP");
     }
-    self_virial = false;    
+    b_self_virial = false;    
     group_1 = all_atoms[1];
     group_2 = all_atoms[2];
   } else {
-    self_virial = true;    
+    b_self_virial = true;    
     group_1 = all_atoms[0];
     group_2 = all_atoms[0];
   }
 
   //make neighbors big enough for all neighbors case
-  vector<unsigned int> neighs(max(group_1.size(), group_2.size());
-
+  neighs.resize(max(group_1.size(), group_2.size()));
+			      
   // And check everything has been read in correctly
   checkRead();
 
-  linkcells.setCutoff( cutoff );  
+
+  linkcells.setCutoff( cutoff );
+
+  addValueWithDerivatives();
+  setNotPeriodic();
+
 }
 
+
+
+  void Virial::prepare() {
+    requestAtoms(group_1);
+    requestAtoms(group_2);
+  }
+  
 void Virial::setupLinkCells(){
-
-  //ensure atoms are ready
-  requestAtoms(group_1);
-  requestAtoms(group_2);
-
-  std::vector<Vector> ltmp_pos( group_1.size() + group_2.size() * self_virial ); 
-  std::vector<unsigned> ltmp_ind( group_1.size() + group_2.size() * self_virial ); 
+  std::vector<Vector> ltmp_pos( group_1.size() + group_2.size() * b_self_virial ); 
+  std::vector<unsigned> ltmp_ind( group_1.size() + group_2.size() * b_self_virial ); 
 
 
   for(unsigned int i = 0; i < group_1.size(); i++) {
-    ltmp_pos[i] = getPosition(group_1[i].index);
-    ltmp_ind[i] = group_1[i].index;
+    ltmp_pos[i] = getPosition(group_1[i]);
+    ltmp_ind[i] = i;
   }  
   unsigned int j;
   for(unsigned int i = group_1.size(); i < ltmp_pos.size(); i++) {
     j = i - group_1.size();
-    ltmp_pos[i] = getPosition(group_2[j].index);
-    ltmp_ind[i] = group_2[j].index;
+    ltmp_pos[i] = getPosition(group_2[j]);
+    ltmp_ind[i] = j;
   }
 
   // Build the lists for the link cells
@@ -150,13 +158,14 @@ void Virial::calculate()  {
   Vector rij;
   double r;
   for(unsigned int i = 0; i < group_1.size(); ++i) {
-    //why???
+
     nn = 1;
-    linkcells.retrieveNeighboringAtoms( getPosition(group_1[i].index), &nn, neighs);
+    neighs[0] = i;
+    linkcells.retrieveNeighboringAtoms( getPosition(group_1[i]), nn, neighs);
     for(unsigned int j = 0; j < nn; ++j) {
-      if(!self_virial && neighs[j] < group_1.size())
+      if(!b_self_virial && neighs[j] < group_1.size())
 	continue;
-      rij = delta(getPosition(group_1[i].index), getPosition(group_2[neighs[j]]));
+      rij = delta(getPosition(group_1[i]), getPosition(group_2[neighs[j]]));
       r = rij.modulo();
       //do thing tiwht r
       log.printf("pairwise = %f\n", r);
@@ -164,8 +173,10 @@ void Virial::calculate()  {
   }
 
   //get pairs 
-  setValue(0)
+  setValue(0);
+
 }
+
 
 }
 }
