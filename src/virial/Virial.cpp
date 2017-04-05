c8eadd2859f392708c24939608641f8504e35c17
Andrew White <white.d.andrew@gmail.com>
2017-04-04 21:49:24 -0400

    Added computation of mean-force

--- src/virial/Virial.cpp
+++ src/virial/Virial.cpp
@@ -57,13 +57,15 @@ private:
   //Neighbor list linkscells to accelerate
   LinkCells linkcells;
   OFile rdffile_;
+  OFile mf_file_;
 
 
-  Grid* rdf_grid_;
+  Grid* gr_grid_;
+  Grid* mean_force_grid_;
 
 
   void setup_link_cells_();
-  double eval_kernel_(const double x, const double r, double* der);
+  double eval_kernel_(const double x, const double r, const double scale, vector<double>& der, vector<double>& der2);
 
 public:
   static void registerKeywords( Keywords& keys );
@@ -99,10 +101,10 @@ void Virial::registerKeywords( Keywords& keys ){
 Virial::Virial(const ActionOptions&ao):
 PLUMED_COLVAR_INIT(ao),
 b_self_virial(false),
-rdf_kernel_support_(1),
 neighs(0),
 linkcells(comm),
-rdf_grid_(NULL)
+gr_grid_(NULL),
+mean_force_grid_(NULL)
 {
   //used for link list
   double cutoff;
@@ -111,7 +113,8 @@ rdf_grid_(NULL)
 
   parse("CUTOFF", cutoff);
 
-  string funcl = getLabel() + ".rdf";
+  string funcl = getLabel() + ".gr";
+  string dfuncl = getLabel() + ".dgr";
   vector<string> gmin, gmax;
   vector<unsigned int> nbin;
   bool b_spline;
@@ -131,9 +134,10 @@ rdf_grid_(NULL)
   //if it is periodic. Since it's not, I'll just 
   //pass some string vectors I have laying around
   //to satisfy the constructor.
-  rdf_grid_ = new Grid(funcl, gnames, gmin, gmax, nbin, b_spline, true, true, gpbc, gnames, gnames);
+  gr_grid_ = new Grid(funcl, gnames, gmin, gmax, nbin, b_spline, true, true, gpbc, gnames, gnames);
+  mean_force_grid_ = new Grid(dfuncl, gnames, gmin, gmax, nbin, b_spline, true, true, gpbc, gnames, gnames);
 
-  rdf_kernel_support_.push_back(ceil(rdf_bw_ / rdf_grid_->getDx()[0]));
+  rdf_kernel_support_.push_back(ceil(rdf_bw_ / gr_grid_->getDx()[0]));
   
 
   parseAtomList("GROUP", all_atoms[0]);
@@ -167,14 +171,19 @@ rdf_grid_(NULL)
 
 
   rdffile_.link(*this);
+  mf_file_.link(*this);
   rdffile_.open("rdf.dat");
+  mf_file_.open("drdf.dat");
 
 }
 
   Virial::~Virial(){
 
-    if(rdf_grid_)
-      free(rdf_grid_);
+    if(gr_grid_) {
+      free(gr_grid_);
+      free(mean_force_grid_);
+    }
+
        
     
   }
@@ -204,9 +213,14 @@ void Virial::setup_link_cells_(){
   linkcells.buildCellLists( ltmp_pos, ltmp_ind, getPbc() );
 }
 
-double Virial::eval_kernel_(const double x, const double r, double* der) {
-    der[0] = 0.75 * 2 * (x - r);
-    return 0.75 * (1 - (x - r) * (x - r) / rdf_bw_ / rdf_bw_ );
+  double Virial::eval_kernel_(const double x, const double r, const double scale, vector<double>& der, vector<double>& der2) {
+    const double A = 35. / 32. * scale;
+    const double u = (x - r) * (x - r);
+    const double b2 = rdf_bw_  * rdf_bw_;
+    const double ib6 = pow(rdf_bw_, -6);
+    der2[0] = ib6 * 6 * A * (5 * u - b2) * ( b2 - u);
+    der[0] = ib6 * 6 * A * (r - x) * (u - b2) * (u - b2);
+    return A * pow(1 - u / b2 ,3);
   }
 
 void Virial::calculate()  {
@@ -215,16 +229,17 @@ void Virial::calculate()  {
   //number of neighbors
   unsigned int nn;
   Vector rij;
-  double r, v, dx;
+  double r, v, dx, gr;
   Grid::index_t ineigh;
   vector<double> rvec(1);
   vector<Grid::index_t> grid_neighs;
   vector<double> grid_der(1);
+  vector<double> grid_der2(1);
   vector<double> x(1);
 
 
   double count = 0;
-  dx = rdf_grid_->getDx()[0];
+  dx = gr_grid_->getDx()[0];
   
   for(unsigned int i = 0; i < group_1.size(); ++i) {
 
@@ -238,14 +253,21 @@ void Virial::calculate()  {
       r = rij.modulo();
       rvec[0] = r;
       
-      grid_neighs = rdf_grid_->getNeighbors(rvec, rdf_kernel_support_);
+      grid_neighs = gr_grid_->getNeighbors(rvec, rdf_kernel_support_);
       for(unsigned k = 0; k < grid_neighs.size();++k){
         ineigh = grid_neighs[k];
-        rdf_grid_->getPoint(ineigh,x);
-        v = eval_kernel_(x[0], r, &grid_der[0]);
-	//rescale them by local volume
-	v /= 4 * 3.14159254 * r * r * dx;
-        rdf_grid_->addValueAndDerivatives(ineigh, v, grid_der);
+        gr_grid_->getPoint(ineigh,x);
+	//rescale by local volume
+	v = 4 * 3.14159254 * r * r * dx;
+        gr = eval_kernel_(x[0], r, v, grid_der, grid_der2);
+        gr_grid_->addValueAndDerivatives(ineigh, v, grid_der);
+	//we want to get d mean force / dr.
+	v = 1 / r * 1 / gr * grid_der[0];
+	//v is mean force derivative
+	//grid_der and grid_der2 are dgr/dr and dgr2/dr2 respectively
+	grid_der2[0] = 1 / r * (1 / r / r / gr * grid_der[0] + 1 / r / gr / gr * grid_der[0] * grid_der[0] -  1 / r / gr * grid_der2[0]);
+	//we have an extra r/r^2 term because we can avoid square roots later by putting them into our tabulated function 
+        mean_force_grid_->addValueAndDerivatives(ineigh, v, grid_der2);
       }
       count++;
       //do thing tiwht r
@@ -256,8 +278,11 @@ void Virial::calculate()  {
   setValue(0);
   
   rdffile_.rewind();
-  rdf_grid_->scaleAllValuesAndDerivatives(1 / count);
-  rdf_grid_->writeToFile(rdffile_);
+  mf_file_.rewind();
+  gr_grid_->scaleAllValuesAndDerivatives(1 / count);
+  mean_force_grid_->scaleAllValuesAndDerivatives(1 / count);
+  gr_grid_->writeToFile(rdffile_);
+  mean_force_grid_->writeToFile(mf_file_);
 }
 
 
